060c44d3a4eac7968a112d42038f5b48ea2e5398
theaniketgiri <theaniketgiri@gmail.com>
2024-07-16 08:34:05 +0530

    Auth

---
+++ lib/db.ts
@@ -0,0 +1,20 @@
+import { PrismaClient } from '@prisma/client'
+
+declare global {
+  var prisma: PrismaClient | undefined
+}
+
+const prismaClientSingleton = () => {
+  return new PrismaClient({
+    log: ['query', 'info', 'warn', 'error'],
+    datasources: {
+      db: {
+        url: process.env.DATABASE_URL,
+      },
+    },
+  })
+}
+
+export const db = globalThis.prisma ?? prismaClientSingleton()
+
+if (process.env.NODE_ENV !== 'production') globalThis.prisma = db
